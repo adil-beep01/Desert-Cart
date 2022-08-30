f9389d8ada49ba75a31b87ecd3f92468cc612bdc
Adil Yaseen <83512772+adil-beep01@users.noreply.github.com>
2022-08-30 13:21:53 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,14 @@
+# input = "{\"time\":\"2022-06-17T05:52:39.787Z\"}"
+# parser(input) => "2022-06-17 07:54:13 +0200"
+
+require 'json'
+require 'time'
+def parser(input)
+  date = JSON.parse(input)
+  time = date['time']
+  utc_time = Time.parse(time) + 2 * 60 * 60
+  puts utc_time
+end
+parser('{"time":"2022-06-17T05:52:39.787Z"}')
+
+# .utc.in_time_zone("Pakistan Standard Time")
---
+++ solution-01.rb
@@ -0,0 +1,4 @@
+def reverse_string(str)
+  puts str.reverse
+end
+reverse_string('hello')
